e5c2287c3f7dbddb58459e37241d8514e97f731f
Nick West <mail@nickwe.st>
2016-10-11 23:57:31 -0400

    routes established, climb on!

---
+++ imports/ui/layouts/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Navigation } from '../components/navigation.jsx';
+
+export const App = ({ children }) => (
+  <div>
+    <Navigation />
+    {children}
+  </div>
+);
+
+App.propTypes = {
+  children: React.PropTypes.object,
+};
