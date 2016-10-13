5e30a96cedf8563f07b56c59b617f8c3864f7792
Nick West <mail@nickwe.st>
2016-10-13 10:59:22 -0400

    html5 geolocation added

--- imports/ui/pages/user.jsx
+++ imports/ui/pages/user.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import SimpleMapPage from '../map/Map';
 
+
+
 export const User = ({ params, location }) => (
   <div className="container">
     <h1>Hello, {params.name} </h1>
+
     <h2>Your current location: latitude: {location.query.lat}, longitude: {location.query.lng}.</h2>
 
     <SimpleMapPage />
-
   </div>
 );
 
