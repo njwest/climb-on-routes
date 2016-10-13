ad326311600eb66d02ad83d45457eb0be1b04ea8
Nick West <mail@nickwe.st>
2016-10-13 12:00:29 -0400

    implemented marker and styles

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -6,7 +6,7 @@ import { App } from '../../ui/layouts/app.jsx';
 import { Index } from '../../ui/components/index.jsx';
 import { One } from '../../ui/pages/one.jsx';
 import { Two } from '../../ui/pages/two.jsx';
-
+import { Marker } from '../../ui/pages/Marker';
 import { User } from '../../ui/pages/user.jsx';
 
 import { NotFound } from '../../ui/pages/not-found.jsx';
@@ -25,8 +25,8 @@ Meteor.startup(() => {
 
           {/* /user/:name Routes, url sends User Name, Latitude and Longitude values to the screen*/}
           {/* Check out the "import { User } from '../../ui/pages/user.jsx' " statement on line 10 of this file^^^*/}
-          <Route path="/:name" component={User} />
-
+          <Route path="/user/:name" component={User} />
+          <Route path="/marker" component={Marker} />
         </Route>
 
         <Route path="*" component={NotFound} />
